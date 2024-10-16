45bbfdd09ebc2f199e7b48a999d24dbd129d3cb6
Kunal <kunalkhan370@gmail.com>
2024-10-16 20:55:21 +0530

    Get Book function added

--- src/app/book.routes.js
+++ src/app/book.routes.js
@@ -7,6 +7,32 @@ const Genre = require('../models/Genre')
 const Book = require('../models/Book')
 const router = express.Router()
 
+router.get('/:book', auth, async (req, res) => {
+    try{
+        const book = await Book.findById(req.params.book)
+        if(!book){
+            return res.status(404).json({
+                code: 404,
+                message: 'Book not found'
+            })
+        }
+        const authors = await book.getAuthors()
+        const genres = await book.getGenres()
+
+        return res.status(200).json({
+            code: 200,
+            message: 'Request Complete!',
+            data: {...book.toJSON(), authors, genres}
+        })
+
+    }catch(e){
+        debug.error(e)
+        return res.status(500).json({
+            code: 500,
+            message: e._message ? e._message : 'Required failed!'
+        })
+    }
+})
 
 router.post('/', auth, async (req, res) => {
     try{
